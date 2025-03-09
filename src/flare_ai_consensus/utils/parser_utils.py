28d392f3b4cae072e93d5bb9452c088e69896dec
michael-tyl <mliuty2016@gmail.com>
2025-03-08 17:02:30 -0800

    remove print statement

--- src/flare_ai_consensus/utils/parser_utils.py
+++ src/flare_ai_consensus/utils/parser_utils.py
@@ -1,6 +1,5 @@
 def parse_chat_response(response: dict) -> str:
     """Parse response from chat completion endpoint"""
-    print(response)
     return response.get("choices", [])[0].get("message", {}).get("content", "")
 
 
